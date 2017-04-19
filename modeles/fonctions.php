4f98d93bf42389462e6460701f74b990b692f056
TimotheeFeret <tim.feret@sfr.fr>
2017-04-19 12:12:32 +0200

    Accès Bdd, classe enfant.

--- modeles/fonctions.php
+++ modeles/fonctions.php
@@ -1,18 +1,23 @@
 <?php
+	require_once "../modeles/enfant.php";
 	require_once "../config/connexion_bd.php";
-
 	/**
 	* 
 	*/
 	class Fonctions
 	{
+		
 		public function add()
 		{
+
 			// Récupération des attributs de l'objet à ajouter.
 			$attributs = get_object_vars ( $this );
-			$nomTable = $this->$nomTable;
-			$req = $bdd->query(" INSERT INTO $nomTable () VALUES (); ");
-			$req->closeCursor(); // Termine le traitement de la requête.
+			/*var_dump($attributs['id'], $attributs['nom'], $attributs['prenom'], $attributs['date_naissance']);*/
+			$nomTable = Enfant::nomTable;
+			$req = DB::connect()->exec("
+				INSERT INTO $nomTable (famille_id, section_id, nom, prenom, date_naissance)
+				VALUES(1, 1, '".$attributs['nom']."', '".$attributs['prenom']."',  '".$attributs['date_naissance']."');
+			");
 		}
 		public function update()
 		{
@@ -21,7 +26,7 @@
 		public function delete()
 		{
 			$id = $this->id;
-			$nomTable = $this->$nomTable;
+			$nomTable = Enfant::nomTable;
 			$req = $bdd->query(" DELETE FROM $nomTable WHERE id = $id; ");
 			$req->closeCursor(); // Termine le traitement de la requête.
 		}
@@ -29,9 +34,9 @@
 		public function getAll()
 		{
 			$attributs = get_object_vars ( $this );
-			var_dump($attributs);
-			var_dump($this);
-			$nomTable = self::nomTable;
+			$nomTable = Enfant::nomTable;
+			// TODO - APPELER LA VARIABLE BDD DU FICHIER connexion_bd.php
+			$bdd = new PDO('mysql:host=localhost;dbname=apero;charset=utf8', 'root', '');
 			$req = $bdd->query(" SELECT * FROM $nomTable; ");
 			$donnees = $req->fetchAll(PDO::FETCH_ASSOC);
 			$req->closeCursor(); // Termine le traitement de la requête
--- test/enfant_test.php
+++ test/enfant_test.php
@@ -1,6 +1,12 @@
 <?php
 	require_once "../modeles/enfant.php";
-	$enfant = new Enfant(1);
-	var_dump(($enfant));
-	echo $enfant->getAll();
+	/*echo '// Création d\'un Enfant avec un id.';
+	$enfant1 = new Enfant(1);
+	var_dump($enfant1);*/
+	echo '// Création d\'un enfant avec nom, prenom, date_naissance.';
+	$enfant2 = new Enfant('nom_Toto', 'Toto', '2017-04-01');
+	var_dump($enfant2);
+	$enfant2->add();
+	/*echo '// Récupération de tous les enfants dans un tableau.';
+	var_dump($enfant1->getAll());*/
 ?>
