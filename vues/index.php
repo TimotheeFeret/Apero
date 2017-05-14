8ffb8f4af2b6e055a0f6ea681b0351ca831c5833
PÉPIN Jérémy <informatique3@pesage-act.com>
2017-05-14 16:44:42 +0200

    Ajout de l'écran d'acceuil

--- vues/index.php
+++ vues/index.php
@@ -1,17 +1,119 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title></title>
-    <?php include_once 'head.php'; ?>
+    <title>Acceuil</title>
+    <?php include_once $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/vues/head.php'; ?>
 </head>
 
 <body>
-    <?php include_once 'topbar.php'; ?>
+<?php include_once $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/vues/topbar.php'; ?>
 
-    <main class="container">
-    </main>
+<main class="container">
+    <h1 class="center">Acceuil</h1>
 
-    <?php include_once 'footer.php'; ?>
+    <div class="row center">
+        <div class="col s1"></div>
+        <a href="vues/famille/list.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/famille.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Familles</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="vues/etablissement/list.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/etablissement.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Établissements</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="vues/livre/list.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/livre.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Livres</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="vues/stock/list.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/stock.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Stock</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="vues/historique/list.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/historique.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Historique</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <div class="col s1"></div>
+    </div>
+
+    <div class="row center">
+        <div class="col s4"></div>
+        <a href="vues/depot/add.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/depot.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Dépôt</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="vues/achat/add.php">
+            <div class="col s2">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="/apero/assets/img/achat.png" class="btn-img">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">Achat</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <div class="col s4"></div>
+    </div>
+
+
+    </div>
+</main>
+
+<?php include_once $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/vues/footer.php'; ?>
 </body>
-
 </html>
